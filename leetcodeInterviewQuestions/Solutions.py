f12e2defc4a2c541f0675c8d106c10ba3aa16697
Stealeristaken <arda-orcun@hotmail.com>
2023-08-18 23:51:02 +0300

    q5 added

--- leetcodeInterviewQuestions/Solutions.py
+++ leetcodeInterviewQuestions/Solutions.py
@@ -137,3 +137,25 @@ class Solution:
                 return self.kth(a[:ia], b, k)
             else:
                 return self.kth(a, b[:ib], k)
+
+####################################################################################################################################
+#Question 5 - Longest Palindrome
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        res = ""
+
+        l = 0
+        r = 1
+
+        while r <= len(s):
+            if s[l:r] != s[l:r][::-1]:
+                if l > 0 and s[l - 1:r] == s[l - 1:r][::-1]:
+                    l -= 1
+                else:
+                    while l < r and s[l:r] != s[l:r][::-1]:
+                        l += 1
+            res = s[l:r] if len(res) < len(s[l:r]) else res
+            r += 1
+        return res
+
