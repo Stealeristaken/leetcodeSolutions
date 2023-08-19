e7e4aa95ddaf3ba77ef66390549929a4ae2a9fc7
Stealeristaken <arda-orcun@hotmail.com>
2023-08-19 21:06:05 +0300

    Q12 added

--- leetcodeInterviewQuestions/Solutions.py
+++ leetcodeInterviewQuestions/Solutions.py
@@ -314,3 +314,20 @@ class Solution:
                     m=a
                 l+=1
         return m
+
+####################################################################################################################################
+# Question 11 - Integer to Roman
+
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        Roman=""
+        storeIntRoman=[[1000, "M"], [900, "CM"], [500, "D"], [400, "CD"], [100, "C"], [90, "XC"], [50, "L"], [40, "XL"], [10, "X"], [9, "IX"], [5, "V"], [4, "IV"], [1, "I"]]
+        for i in range(len(storeIntRoman)):
+            while num >= storeIntRoman[i][0]:
+                Roman+=storeIntRoman[i][1]
+                num-=storeIntRoman[i][0]
+        return Roman
