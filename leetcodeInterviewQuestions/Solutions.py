fc91874aac7309291953b421eec49dcb1d721b99
Stealeristaken <arda-orcun@hotmail.com>
2023-08-19 20:47:35 +0300

    q added

--- leetcodeInterviewQuestions/Solutions.py
+++ leetcodeInterviewQuestions/Solutions.py
@@ -253,3 +253,17 @@ class Solution:
         value = max(-(2 ** 31), value)
 
         return value
+
+####################################################################################################################################
+# Question 9 - Palindrome Number
+
+class Solution(object):
+    def isPalindrome(self, x):
+        if x < 0:
+            return False
+        input_num=x
+        new_num=0
+        while x>0:
+            new_num=new_num*10+x%10
+            x//=10
+        return new_num==input_num
