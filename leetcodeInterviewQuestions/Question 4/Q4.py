ad8be2e030346cfaf16892202012b5a776dd9b19
Stealeristaken <arda-orcun@hotmail.com>
2023-09-21 22:07:29 +0300

    q added

---
+++ leetcodeInterviewQuestions/Question 4/Q4.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        merged = []
+        i, j = 0, 0
+
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] < nums2[j]:
+                merged.append(nums1[i])
+                i += 1
+            else:
+                merged.append(nums2[j])
+                j += 1
+
+        while i < len(nums1):
+            merged.append(nums1[i])
+            i += 1
+
+        while j < len(nums2):
+            merged.append(nums2[j])
+            j += 1
+
+        mid = len(merged) // 2
+        if len(merged) % 2 == 0:
+            return (merged[mid - 1] + merged[mid]) / 2
+        else:
+            return merged[mid]
