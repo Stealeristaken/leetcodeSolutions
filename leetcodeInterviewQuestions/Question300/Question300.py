0fdd713b07b51a683bb34202633baf84c13c4c43
Stealeristaken <arda-orcun@hotmail.com>
2024-01-05 11:51:49 +0300

    Question 300 added

---
+++ leetcodeInterviewQuestions/Question300/Question300.py
@@ -0,0 +1,9 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n=len(nums)
+        lis=[1]*n
+        for i in range (1,n):
+            for j in range(0,i):
+                if nums[i]>nums[j] and lis[i]<lis[j]+1:
+                    lis[i]=lis[j]+1
+        return max(lis)
