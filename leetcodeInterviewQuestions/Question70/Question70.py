c7d046b4f0f403307cbb22465c12f9d9850c8094
Stealeristaken <arda-orcun@hotmail.com>
2024-01-18 07:45:40 +0300

    Q70 alternative added cuzed by daily

--- leetcodeInterviewQuestions/Question70/Question70.py
+++ leetcodeInterviewQuestions/Question70/Question70.py
@@ -9,3 +9,14 @@ class Solution:
             steps[2],steps[1],steps[0]=currentStep,steps[2],steps[1]
 
         return steps[2]
+
+#Another One Bites to Dust
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        oneAgo = 1
+        twoAgo = 1
+        for x in range(n - 1):
+            oneAgo, twoAgo = oneAgo + twoAgo, oneAgo
+        return oneAgo
