144ce600e0cf762641eb75057dcdec948cf75c98
Stealeristaken <arda-orcun@hotmail.com>
2024-02-01 09:56:18 +0300

    daily q updated

--- leetcodeInterviewQuestions/Question2966/Question2966.py
+++ leetcodeInterviewQuestions/Question2966/Question2966.py
@@ -11,3 +11,14 @@ class Solution:
                 ans.append([nums[i],nums[i+1],nums[i+2]])
 
         return ans        
+  
+  
+  
+  # Faster and Better Solution:
+class Solution:
+      def divideArray(self, nums: List[int], k: int) -> List[List[int]]:
+        a=sorted(nums)
+        b=max(i-j for i,j in zip(a[2::3],a[::3]))
+        if b>k:
+            return []
+        return zip(a[::3],a[1::3],a[2::3])
