74b76cd8bbe4b6fa226134ebe0c7270f56dd772a
Stealeristaken <arda-orcun@hotmail.com>
2024-02-06 14:13:58 +0300

    Daily Added

--- leetcodeInterviewQuestions/Question49/Question49.py
+++ leetcodeInterviewQuestions/Question49/Question49.py
@@ -13,3 +13,18 @@ class Solution:
                 f.append(strs[new])
             result.append(f)
         return result
+    
+    
+    
+################################################################
+
+f = open('user.out', 'w')
+
+
+for strs in map(loads, stdin):
+    w = defaultdict(list)
+    for s in strs:
+        w[''.join(sorted(s))].append(s)
+    
+    print(str(w.values())[12:-1].replace("'", '"').replace(" ", ""), file=f)
+exit()
