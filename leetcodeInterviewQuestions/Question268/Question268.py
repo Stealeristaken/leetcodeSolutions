260a7ce65f52ddb24f22256b41e44bec7544c38c
Stealeristaken <arda-orcun@hotmail.com>
2024-02-20 06:46:32 +0300

    daily q added

---
+++ leetcodeInterviewQuestions/Question268/Question268.py
@@ -0,0 +1,14 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        ideal = [i for i in range (0:i
+                                   len(nums)+1)]
+        nums=list(set(ideal)-set(nums))
+
+
+
+        with open("user.out", "w") as file:
+    for i in stdin:
+        i = list(map(int, i.rstrip()[1:-1].split(',')))
+        n = len(i)
+        file.write(str(sum(range(0,n+1))-sum(i))+"\n")
+exit(0)
