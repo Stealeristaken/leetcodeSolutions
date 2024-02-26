ec3565e0134ca9c358bce580f30eabbed25a0803
Stealeristaken <arda-orcun@hotmail.com>
2024-02-26 10:10:17 +0300

    new sol added for daily q

--- leetcodeInterviewQuestions/Question100/Question100.py
+++ leetcodeInterviewQuestions/Question100/Question100.py
@@ -18,3 +18,21 @@ class Solution:
             return (check_val and check_left and check_right)
 
         return (is_equal(p, q))
+    
+    
+    
+    
+    
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution2:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p is None and q is None:
+            return True
+        if p is None or q is None:
+            return False
+        return (p.val == q.val and self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right))
