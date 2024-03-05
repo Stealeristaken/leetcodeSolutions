5c99a1109848247f9bdff3344afe6a9fc6af6c2a
Stealeristaken <arda-orcun@hotmail.com>
2024-03-05 09:37:28 +0300

    daily added

---
+++ leetcodeInterviewQuestions/Question1750/Question1750.py
@@ -0,0 +1,29 @@
+class Solution:
+      def minimumLength(self, s: str) -> int:
+        left, right = 0, len(s) - 1
+        while left < right and s[left] == s[right]:
+            char = s[left]
+            while left <= right and s[left] == char:
+                left += 1
+            while right >= left and s[right] == char:
+                right -= 1
+        return right - left + 1
+  
+  
+  
+  
+###########################
+
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        l, r = 0, len(s) - 1
+        while l < r and s[l] == s[r]:
+            char = s[l]
+            l += 1
+            r -= 1
+            while l <= r and s[l] == char:
+                l += 1
+            while l <= r and s[r] == char:
+                r -= 1
+        
+        return r - l + 1
