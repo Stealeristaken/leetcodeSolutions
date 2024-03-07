88bb6cc4689d22783ff822f984387ed9e782a841
Stealeristaken <arda-orcun@hotmail.com>
2024-03-07 05:25:15 +0300

    daily added

---
+++ leetcodeInterviewQuestions/Question876/Question786.py
@@ -0,0 +1,27 @@
+class Solution:
+      def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+  
+  
+  
+#############################################
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None or head.next is None:
+            return head
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
