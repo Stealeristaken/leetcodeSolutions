4a7dd113a89f87c4f7565351749ac5e8996b7fa3
Stealeristaken <arda-orcun@hotmail.com>
2024-03-11 06:51:10 +0300

    deafultdict usage added

--- leetcodeInterviewQuestions/Question791/Question791.py
+++ leetcodeInterviewQuestions/Question791/Question791.py
@@ -12,3 +12,23 @@ class Solution:
             result.append(c * count[c])
         return "".join(result)
   
+####################
+
+
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        counter = defaultdict(int)
+        for c in s:
+            counter[c] += 1
+
+        sb = []
+        for o in order:
+            for i in range(counter[o]):
+                sb.append(o)
+            counter[o] = 0
+
+        for key in counter:
+                for i in range(counter[key]):
+                    sb.append(key)
+        
+        return ''.join(sb)
