991bb1fa31cf20b36e8b7aae12d83086461bc65d
Stealeristaken <arda-orcun@hotmail.com>
2024-03-14 07:03:21 +0300

    a different side added

--- leetcodeInterviewQuestions/Question930/Question930.py
+++ leetcodeInterviewQuestions/Question930/Question930.py
@@ -31,3 +31,21 @@ class Solution:
 
             res += count
         return res
+  
+  
+##### Different Side of the Problem #####
+
+
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        def atMost(goal):
+            if goal < 0: return 0
+            res = i = 0
+            for j in range(len(nums)):
+                goal -= nums[j]
+                while goal < 0:
+                    goal += nums[i]
+                    i += 1
+                res += j - i + 1
+            return res
+        return atMost(goal) - atMost(goal - 1)
