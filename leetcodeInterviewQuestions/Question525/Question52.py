a1b727a8876e261025662e7090efa6b1e4e07ac5
Stealeristaken <arda-orcun@hotmail.com>
2024-03-16 07:40:51 +0300

    sys.out method added

--- leetcodeInterviewQuestions/Question525/Question52.py
+++ leetcodeInterviewQuestions/Question525/Question52.py
@@ -19,3 +19,22 @@ class Solution:
   
   
   
+###### sys.out solution as usual ######
+
+
+sys.stdout = open('user.out', 'w')
+for nums in map(loads, stdin):
+    totalsum,hashmap=0,{0:-1}
+    res,diff=0,0
+    for i in range(len(nums)):
+        if(nums[i]==0):
+            totalsum-=1
+        else:
+            totalsum+=1
+        try:
+            diff=i-hashmap[totalsum]
+            if(diff>res):
+                res=diff
+        except:
+            hashmap[totalsum]=i
+    print(res)
