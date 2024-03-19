dff618212bc39a44097aaa7deb7c9acad27cb94e
Stealeristaken <arda-orcun@hotmail.com>
2024-03-19 21:05:30 +0300

    daily added

---
+++ leetcodeInterviewQuestions/Question621/Question621.py
@@ -0,0 +1,11 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+      task_map = [0]*26
+      for task in tasks:
+            task_map[ord(task) - ord('A')] += 1
+      task_map.sort()
+      max_val = task_map[25] - 1
+      idle_slots = max_val * n
+      for i in range(24, -1, -1):
+            idle_slots -= min(task_map[i], max_val)
+      return len(tasks) + max(0, idle_slots)
