316dfbbd3d65e49f7166f37e1b6b7d3a30221175
Stealeristaken <arda-orcun@hotmail.com>
2024-03-19 21:06:01 +0300

    daily more optimized

--- leetcodeInterviewQuestions/Question621/Question621.py
+++ leetcodeInterviewQuestions/Question621/Question621.py
@@ -9,3 +9,17 @@ class Solution:
       for i in range(24, -1, -1):
             idle_slots -= min(task_map[i], max_val)
       return len(tasks) + max(0, idle_slots)
+
+
+
+
+
+#############
+
+from queue import PriorityQueue
+from collections import Counter
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        l = Counter(tasks)
+        return max((n+1)*(max(l.values())-1) + len([x for x in l.values() if x == max(l.values())]) , len(tasks))
+        
