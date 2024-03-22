24fb4d73b96ba579bd072a1fec2ea5b382ff87dd
Stealeristaken <arda-orcun@hotmail.com>
2024-03-22 08:41:25 +0300

    daily added

--- leetcodeInterviewQuestions/Question234/Question234.py
+++ leetcodeInterviewQuestions/Question234/Question234.py
@@ -31,9 +31,21 @@ class Solution:
                 return False
         return True
         
-        
-       
+############################################################################################################################################################################
 
+
+
+
+import sys
+
+with open('user.out', 'w') as f:
+    while True:
+        line = sys.stdin.readline().strip()[1:-1]
+        if not line:
+            break
+        f.write('true\n' if line == line[::-1] else 'false\n')
+
+exit(0)
 
         
         
