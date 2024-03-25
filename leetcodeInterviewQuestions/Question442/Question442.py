1787d5e5bcf293bb7eca5ad6956f5940a1f67d08
Stealeristaken <arda-orcun@hotmail.com>
2024-03-25 06:51:06 +0300

    map way added

--- leetcodeInterviewQuestions/Question442/Question442.py
+++ leetcodeInterviewQuestions/Question442/Question442.py
@@ -7,3 +7,20 @@ class Solution:
                   else:
                    nums[abs(nums[i])-1] *= -1
             return res
+      
+      
+      
+########### MAP SOLUTION ################
+
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        duplicatemap={}
+        for i in nums:
+            if(i in duplicatemap):
+                duplicatemap[i]+=1
+            else:
+                duplicatemap[i]=1
+        return [k for k,v in duplicatemap.items() if v>=2]
+            
+            
+        
