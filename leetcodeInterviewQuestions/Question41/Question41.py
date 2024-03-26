1ead1c2bb1bf40e27f15efe425ec283c26d334d6
Stealeristaken <arda-orcun@hotmail.com>
2024-03-26 10:02:22 +0300

    daily added

--- leetcodeInterviewQuestions/Question41/Question41.py
+++ leetcodeInterviewQuestions/Question41/Question41.py
@@ -9,9 +9,29 @@ class Solution:
 
             index+=1
 
+
         index=1
         while index<len(nums):
             if index!=nums[index]:
                 return index
             index+=1
         return nums[-1]+1
+    
+    
+    
+    
+    
+    
+    
+    #### # # # # #  another way to solve question 
+    
+    
+    
+f = open("user.out", 'w')
+for line in stdin:
+    s = set(map(int, line.rstrip()[1:-1].split(',')))
+    ans = 1
+    while ans in s:s
+        ans += 1
+    print(ans, file=f)
+exit(0)
