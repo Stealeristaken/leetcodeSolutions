126a14467c574eced8916c56bd8515890da1e1ef
Stealeristaken <arda-orcun@hotmail.com>
2024-03-29 10:15:42 +0300

    daily added

--- leetcodeInterviewQuestions/Question2962/Question2962.py
+++ leetcodeInterviewQuestions/Question2962/Question2962.py
@@ -14,3 +14,30 @@ class Solution:
                 start += 1
             ans += start
         return ans
+  
+  
+  
+  
+  
+########### Another Solution ###########  
+
+
+class Solution:
+    def countSubarrays(self, nums: List[int], k: int) -> int:
+        ma = nums[0]
+        ps = [0]
+        n = len(nums)
+        for i in range(1, n):
+            if nums[i] > ma:
+                ma = nums[i]
+                ps = [i]
+            elif nums[i] == ma:
+                ps.append(i)
+        if len(ps) < k:
+            return 0
+        ans = 0
+        idx = -1
+        for l in range(len(ps)-k+1):
+            ans += (n - ps[l+k-1]) * (ps[l] - idx)
+            idx = ps[l]
+        return ans
