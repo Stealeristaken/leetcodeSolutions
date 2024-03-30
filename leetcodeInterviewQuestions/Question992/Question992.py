19840a3b1ecfbfc6aeb6e9b39580479270962d2d
Stealeristaken <arda-orcun@hotmail.com>
2024-03-30 08:19:50 +0300

    daily another solvation

--- leetcodeInterviewQuestions/Question992/Question992.py
+++ leetcodeInterviewQuestions/Question992/Question992.py
@@ -23,3 +23,25 @@ class Solution:
     
     def subarraysWithKDistinct(self, nums, k):
         return self.countSubarraysWithAtMostKDistinct(nums, k) - self.countSubarraysWithAtMostKDistinct(nums, k - 1)
+  
+  
+############ Another Solution ############
+
+
+
+class Solution:
+    def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
+        def f(counts=[0] * (len(nums) + 1), low=0, high=0, k=k):
+            for num in nums:
+                if not counts[num]:
+                    if (k := k - 1) < 0:
+                        counts[nums[high]] = 0
+                        low = high = high + 1
+                counts[num] += 1
+                if k <= 0:
+                    while counts[(a := nums[high])] > 1:
+                        counts[a] -= 1
+                        high += 1
+                    yield high - low + 1
+
+        return sum(f())  
