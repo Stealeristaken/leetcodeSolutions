f4be6ff1bd6deabd697f10750b5292c50dc3a960
Stealeristaken <arda-orcun@hotmail.com>
2024-04-05 19:23:01 +0300

    daily added

--- leetcodeInterviewQuestions/Question1544/Question1544.py
+++ leetcodeInterviewQuestions/Question1544/Question1544.py
@@ -12,3 +12,21 @@ class Solution:
                 s=arr
             i+=1
         return s
+  
+  
+  
+#################### 
+
+class Solution:
+    def makeGood(self, s: str) -> str:
+        stack = []
+        for char in s:
+            if stack:
+                if char.lower() == stack[-1].lower() and char != stack[-1]:
+                    stack.pop()
+                else:
+                    stack.append(char)
+            else:
+                stack.append(char)
+        return "".join(stack)
+        
