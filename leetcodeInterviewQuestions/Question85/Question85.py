534615be05426835db1b9edc8ba6bfa83d0fc223
Stealeristaken <arda-orcun@hotmail.com>
2024-04-13 20:14:56 +0300

    more fancy method added

--- leetcodeInterviewQuestions/Question85/Question85.py
+++ leetcodeInterviewQuestions/Question85/Question85.py
@@ -23,3 +23,33 @@ class Solution:
                     height[j]=0
             ans=max(ans,mah(height))
         return ans
+    
+    
+    
+######################
+
+
+
+def find_maximal_rectangle(matrix: list[list[str]]) -> int:
+    m = len(matrix)
+    n = len(matrix[0])
+    heights = [0] * (n + 1)
+    best = 0
+    for row in matrix:
+        for col in range(n):
+            heights[col] = heights[col] + 1 if row[col] == '1' else 0
+        stack = [-1]
+        for col in range(n + 1):
+            while heights[col] < heights[stack[-1]]:
+                h = heights[stack.pop()]
+                w = col - stack[-1] - 1
+                best = max(best, h * w)
+            stack.append(col)
+    return best
+if __name__ == '__main__':
+    import json, sys
+    with open('user.out', 'w') as f:
+        for matrix in map(json.loads, sys.stdin):
+            result = find_maximal_rectangle(matrix)
+            print(result, file=f)
+    sys.exit()
