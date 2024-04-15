9cb3d107baa5363aa82f9cff88a6041bdca49767
Stealeristaken <arda-orcun@hotmail.com>
2024-04-15 09:15:32 +0300

    another solution added

--- leetcodeInterviewQuestions/Question129/Question129.py
+++ leetcodeInterviewQuestions/Question129/Question129.py
@@ -17,3 +17,24 @@ class Solution:
             return dfs(node.left, num) + dfs(node.right, num)
         
         return dfs(root, 0)
+  
+  
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        if not root: return 0
+
+        sum_root_to_leaf_numbers = 0
+        def preorder(node, curr_sum):
+            if not node: return 0
+
+            nonlocal sum_root_to_leaf_numbers
+            curr_sum = curr_sum * 10 + node.val
+
+            if not(node.left or node.right):
+                sum_root_to_leaf_numbers += curr_sum
+            
+            preorder(node.left, curr_sum)
+            preorder(node.right, curr_sum)
+        
+        preorder(root, 0)
+        return sum_root_to_leaf_numbers
