c811aa23152e6bca7aeafe8e67e8824e67081707
Stealeristaken <arda-orcun@hotmail.com>
2024-04-22 09:05:10 +0300

    daily added

--- leetcodeInterviewQuestions/Question752/Question752.py
+++ leetcodeInterviewQuestions/Question752/Question752.py
@@ -39,3 +39,61 @@ class Solution:
 
 
 
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        # bfs from target search
+        # deadend elimite approach
+        # use a visited to avoid duplicate
+        # 0000 - [1000, 0100, 0010, 0001, 9000, 0900, 0090, 0009]
+
+        if target == "0000":
+            return 0
+        if "0000" in (deadends := set(deadends)):
+            return -1
+        transitions = {str(i): (str((i + 1) % 10), str((i - 1) % 10)) for i in range(10)}
+        start, end = {"0000"}, {target}
+        deadends.add("0000")
+        deadends.add(target)
+        turns = 1
+        while start and end:
+            if len(start) > len(end):
+                start, end = end, start
+            temp = set()
+            for state in start:
+                for i in range(4):
+                    for j in transitions[state[i]]:
+                        new_state = state[:i] + j + state[i+1:]
+                        if new_state in end:
+                            return turns
+                        if new_state not in deadends:
+                            deadends.add(new_state)
+                            temp.add(new_state)
+            start = temp
+            turns += 1
+        return -1
+
+        # visited = set()
+        # for deadend in deadends:
+        #     visited.add((int(deadend[0]), int(deadend[1]), int(deadend[2]), int(deadend[3])))
+        # q = collections.deque([(0, 0, 0, 0, 0)])
+        # t_a, t_b, t_c, t_d = int(target[0]), int(target[1]), int(target[2]), int(target[3])
+
+        # while q:
+        #     a,b,c,d,step = q.popleft()
+        #     if (a, b, c, d) in visited or (a, b, c, d) in deadends:
+        #         continue
+        #     if a == t_a and b == t_b and c == t_c and d == t_d:
+        #         return step
+
+        #     visited.add((a, b, c, d))
+        #     q.append(((a + 11) % 10, b, c, d, step + 1))
+        #     q.append(((a + 9) % 10, b, c, d, step + 1))
+        #     q.append((a, (b + 11) % 10, c, d, step + 1))
+        #     q.append((a, (b + 9) % 10, c, d, step + 1))
+        #     q.append((a, b, (c + 11) % 10, d, step + 1))
+        #     q.append((a, b, (c + 9) % 10, d, step + 1))
+        #     q.append((a, b, c, (d + 11) % 10, step + 1))
+        #     q.append((a, b, c, (d + 9) % 10, step + 1))
+            
+        
+        # return -1
