9bcd74e9cce3659bc464e704da835956ae9bcb8f
Stealeristaken <arda-orcun@hotmail.com>
2024-04-23 13:23:56 +0300

    daily added no motiv

---
+++ leetcodeInterviewQuestions/Question310/Question310.py
@@ -0,0 +1,20 @@
+from typing import List
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        if n == 1:
+            return [0]
+        adj = [set() for _ in range(n)]
+        for u, v in edges:
+            adj[u].add(v)
+            adj[v].add(u)
+        leaves = [i for i in range(n) if len(adj[i]) == 1]
+        while n > 2:
+            n -= len(leaves)
+            new_leaves = []
+            for leaf in leaves:
+                neighbor = adj[leaf].pop()
+                adj[neighbor].remove(leaf)
+                if len(adj[neighbor]) == 1:
+                    new_leaves.append(neighbor)
+            leaves = new_leaves
+        return leaves
