999e3b09d6f449812ded976539c66ae583e19d0f
Stealeristaken <arda-orcun@hotmail.com>
2024-04-24 09:02:21 +0300

    daily added

--- leetcodeInterviewQuestions/Question1137/Question1137.py
+++ leetcodeInterviewQuestions/Question1137/Question1137.py
@@ -8,3 +8,23 @@ class Solution:
             for _ in range(n - 2):
                   a, b, c = b, c, a + b + c
             return c
+      
+      
+      
+#### 
+
+
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n <= 2:
+            return 0 if n == 0 else 1
+
+        dp = [0 for _ in range(n + 1)]
+        dp[1], dp[2] = 1, 1
+
+        for i in range(3, n+1):
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+
+        return dp[n]
+        
+        
