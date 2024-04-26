2cb9a394290243e88e74fbd6b36ce40f8cf9fd92
Stealeristaken <arda-orcun@hotmail.com>
2024-04-26 08:03:11 +0300

    more optimized way added

--- leetcodeInterviewQuestions/Question1289/Question1289.py
+++ leetcodeInterviewQuestions/Question1289/Question1289.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution:
     def minFallingPathSum(self, grid: List[List[int]]) -> int:
         if len(grid)==1:
@@ -23,4 +25,23 @@ class Solution:
             res=min(res,dfs(0,i))
         return res
 
-        
+############# 
+
+
+
+class Solution:
+    def minFallingPathSum(self, grid: List[List[int]]) -> int:
+        N = len(grid)
+        DP = grid[0]
+
+        for i in range(1, N):
+            indx1 = DP.index(min(DP))
+            indx2 = DP.index(min(DP[:indx1] + DP[indx1+1:]))
+            for j in range(N):
+                if j != indx1:
+                    grid[i][j] += DP[indx1]
+                else:
+                    grid[i][j] += DP[indx2]
+            DP = grid[i]
+
+        return min(DP)
