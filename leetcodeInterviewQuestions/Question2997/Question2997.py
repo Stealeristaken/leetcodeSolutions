c101c154430b777494d042e22e126d66c57bc14b
Stealeristaken <arda-orcun@hotmail.com>
2024-04-29 07:46:06 +0300

    daily added

--- leetcodeInterviewQuestions/Question2997/Question2997.py
+++ leetcodeInterviewQuestions/Question2997/Question2997.py
@@ -8,3 +8,23 @@ class Solution:
             temp ^= i
         temp ^= k
         return bin(temp).count('1')
+  
+  
+  
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        ops = 0
+        n = len(nums)
+        hasones = bool(max(nums) > 0)
+        while k or hasones:
+            numones = 0
+            hasones = False
+            for i in range(n):
+                numones ^= (nums[i] % 2)
+                nums[i] >>= 1
+                if nums[i]:
+                    hasones = True
+            if numones != k % 2:
+                ops += 1
+            k >>= 1
+        return ops
