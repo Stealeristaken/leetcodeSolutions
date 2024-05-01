2e9d2cb508dfd0ac0a8b0def95eb31be21fc9ead
Stealeristaken <arda-orcun@hotmail.com>
2024-05-01 09:28:24 +0300

    daily

--- leetcodeInterviewQuestions/Question2000/Question2000.py
+++ leetcodeInterviewQuestions/Question2000/Question2000.py
@@ -4,3 +4,17 @@ class Solution:
             if char_i == ch:
                 return word[i::-1] + word[i+1:]
         return word
+  
+  
+########################
+
+class Solution:
+    def reversePrefix(self, word: str, ch: str) -> str:
+        x=0
+        for i in range(len(word)):
+            if word[x]==ch:
+                return word[x::-1]+word[x+1:]
+            x+=1
+        return word
+
+        
