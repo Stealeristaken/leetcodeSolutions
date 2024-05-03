40db991321a6eaf07b06e925d60164c5b2cd21da
Stealeristaken <arda-orcun@hotmail.com>
2024-05-03 07:54:41 +0300

    daily added

--- leetcodeInterviewQuestions/Question165/Question165.py
+++ leetcodeInterviewQuestions/Question165/Question165.py
@@ -11,3 +11,25 @@ class Solution:
                   elif v1[i] > v2[i]:
                    return 1
             return 0
+      
+      
+#########################   
+      
+class Solution:
+    def compareVersion(self, version1: str, version2: str) -> int:
+        version1 = version1.split('.')
+        version2 = version2.split('.')
+        ml = min(len(version1), len(version2))
+        for i in range(ml):
+            v1 = int(version1[i])
+            v2 = int(version2[i])
+            if v1 < v2:
+                return -1
+            elif v1 > v2:
+                return 1
+        version = version1 if len(version1) - ml > 0 else version2
+        v = 1 if len(version1) - ml > 0 else 2
+        for i in range(ml, len(version)):
+            if int(version[i]) > 0:
+                return 1 if v == 1 else -1 
+        return 0
