ce8ec4024f2a6d79884958f6c9ee54742ce6f1b3
Stealeristaken <arda-orcun@hotmail.com>
2024-05-05 05:32:46 +0300

    daily

--- leetcodeInterviewQuestions/Question237/Question237.py
+++ leetcodeInterviewQuestions/Question237/Question237.py
@@ -3,7 +3,7 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-
+from typing import ListNode
 class Solution:
     def deleteNode(self, node: ListNode) -> None:
         node.val, node.next = node.next.val, node.next.next
