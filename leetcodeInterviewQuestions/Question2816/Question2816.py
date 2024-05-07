814716de952659e0985505a2165d8e18501d1cbc
Stealeristaken <arda-orcun@hotmail.com>
2024-05-07 20:29:54 +0300

    daily added

--- leetcodeInterviewQuestions/Question2816/Question2816.py
+++ leetcodeInterviewQuestions/Question2816/Question2816.py
@@ -21,3 +21,29 @@ class Solution:
             x, q.next = q.next, p
             p, q = q, x
         return p
+  
+  
+  
+  
+  
+#######################
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    
+    def doubleIt(self, head):
+        curr = head 
+        if curr.val > 4: 
+            head = ListNode(1, head) 
+            # ans = ListNode(1, head)
+        while curr.next: 
+            curr.val = (curr.val * 2 + (curr.next.val > 4)) % 10
+            curr = curr.next 
+        curr.val = (curr.val * 2) % 10
+
+        return head
