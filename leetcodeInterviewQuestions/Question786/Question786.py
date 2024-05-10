a7a8fa256a6c0710941a9bc6e81edf708005936c
Stealeristaken <arda-orcun@hotmail.com>
2024-05-10 09:50:55 +0300

    daily added

--- leetcodeInterviewQuestions/Question786/Question786.py
+++ leetcodeInterviewQuestions/Question786/Question786.py
@@ -17,3 +17,45 @@ class Solution:
             heapq.heappop(heap)
 
         return list(heapq.heappop(heap)[1])
+  
+  
+  
+  
+##################
+
+
+
+class Solution:
+    def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
+        def con(value):
+            nb_smallest_fraction = 0
+            numer = arr[0]
+            denom = arr[-1]
+
+            slow = 0
+            for fast in range(1, len(arr)):
+                while slow < fast and arr[slow] / arr[fast] < value:
+                    if arr[slow] / arr[fast] > numer / denom:
+                        numer, denom = arr[slow], arr[fast]
+
+                    slow += 1
+
+                nb_smallest_fraction += slow
+
+            return nb_smallest_fraction, numer, denom
+
+        l = arr[0] / arr[-1]
+        r = 1
+
+        while l < r:
+            m = (l+r) / 2
+
+            count, numer, denom = con(m)
+
+            if count == k:
+                return [numer, denom]
+
+            if count > k:
+                r = m
+            else:
+                l = m
