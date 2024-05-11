95e88d93659102e85ac2da11fbd3d31c1e623cdc
Stealeristaken <arda-orcun@hotmail.com>
2024-05-11 16:44:14 +0300

    daily another solution added

--- leetcodeInterviewQuestions/Question857/Question857.py
+++ leetcodeInterviewQuestions/Question857/Question857.py
@@ -19,3 +19,37 @@ class Solution:
             if len(maxHeap) == k:
                 result = min(result,total_quality*rate)
         return result
+  
+  
+  
+  
+  
+  
+##########################
+
+
+
+
+import heapq
+class Solution:
+    def mincostToHireWorkers(self, quality: List[int], wage: List[int], k: int) -> float:
+        n = len(wage)
+        workers = [(wage[i]/quality[i], quality[i]) for i in range(n)]
+        workers.sort()
+        total_quality = 0
+        ratio = 0
+        heap = []
+        heapq.heapify(heap)
+        for i in range(k):
+            ratio = workers[i][0]
+            total_quality += workers[i][1]
+            heapq.heappush(heap, -workers[i][1])
+        res = ratio*total_quality
+        for i in range(k, n):
+            ratio = workers[i][0]
+            total_quality += workers[i][1]
+            heapq.heappush(heap, -workers[i][1])
+            quality = -heapq.heappop(heap)
+            total_quality -= quality
+            res = min(res, ratio*total_quality)
+        return res
