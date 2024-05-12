8af26a2e8bad54fd564d8c698120200eaeba9894
Stealeristaken <arda-orcun@hotmail.com>
2024-05-12 05:07:40 +0300

    daily another

--- leetcodeInterviewQuestions/Question2373/Question2373.py
+++ leetcodeInterviewQuestions/Question2373/Question2373.py
@@ -14,3 +14,23 @@ class Solution:
                 res[x][y] = local_max
 
         return res
+  
+  
+  
+  
+################################## 
+
+
+class Solution:
+    def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
+        length = len(grid)
+        res =[]
+        for i in range(length-2):
+            temp=[]
+            for j in range(length-2):
+                maxi = max(grid[i][j],grid[i][j+1],grid[i][j+2],
+                           grid[i+1][j],grid[i+1][j+1],grid[i+1][j+2],
+                           grid[i+2][j],grid[i+2][j+1],grid[i+2][j+2])
+                temp.append(maxi)
+            res.append(temp)
+        return res
