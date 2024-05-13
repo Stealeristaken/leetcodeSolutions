87f9a545b36452aa389f7bfbbfca2cc604b34efd
Stealeristaken <arda-orcun@hotmail.com>
2024-05-13 09:35:43 +0300

    defaultdict method added

--- leetcodeInterviewQuestions/Question861/Question861.py
+++ leetcodeInterviewQuestions/Question861/Question861.py
@@ -19,3 +19,32 @@ class Solution:
                   for j in range(m):
                         res += grid[i][j] * (1 << (m - 1 - j))
             return res
+      
+############################################################################################################      
+      
+from collections import defaultdict      
+class Solution:
+    def matrixScore(self, grid: List[List[int]]) -> int:
+
+        rows,cols = len(grid),len(grid[0])
+        for r in range(rows):
+            if grid[r][0] == 0:
+                for c in range(cols):
+                    if grid[r][c] == 0:
+                        grid[r][c] = 1
+                    else:
+                        grid[r][c] = 0
+
+        counts = defaultdict(int)
+        for c in range(1,cols):
+            for r in range(rows):
+                if grid[r][c] == 0:
+                    counts[c] += 1
+
+        res = rows * (2**(cols-1))
+        for c in range(1,cols):
+            res += max(counts[c],rows - counts[c])*2**(cols - c - 1)
+
+        return res
+
+            
