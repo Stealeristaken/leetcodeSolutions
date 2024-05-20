6f3110fd87df2aee2716f2d53057710e5fbe0763
Stealeristaken <arda-orcun@hotmail.com>
2024-05-20 04:44:52 +0300

    daily add

--- leetcodeInterviewQuestions/Question1863/Question1863.py
+++ leetcodeInterviewQuestions/Question1863/Question1863.py
@@ -15,3 +15,24 @@ class Solution:
         
         dfs(0, 0)
         return res
+
+
+
+# ---------------------------------------------------
+
+
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+
+        res = 0
+        def dfs(i, path):
+            if i == len(nums):
+                nonlocal res
+                res += path
+                return
+            
+            dfs(i+1, path)
+            dfs(i+1, path ^ nums[i])
+        
+        dfs(0, 0)
+        return res
