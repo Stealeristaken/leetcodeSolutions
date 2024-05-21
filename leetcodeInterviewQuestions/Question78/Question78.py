f854c2158fdc01e42fea6b24d9052ef35c295244
Stealeristaken <arda-orcun@hotmail.com>
2024-05-21 08:24:41 +0300

    daily added

--- leetcodeInterviewQuestions/Question78/Question78.py
+++ leetcodeInterviewQuestions/Question78/Question78.py
@@ -1,3 +1,4 @@
+from typing import List
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         ans = []
@@ -12,3 +13,26 @@ class Solution:
             solve (i+1, curr)
         solve(0,[])
         return ans
+    
+    
+######
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        subset = []
+
+        def dfs(i):
+            if i >= len(nums):
+                res.append(subset.copy())
+                return
+            # decision to include nums[i]
+            subset.append(nums[i])
+            dfs(i + 1)
+            # decision NOT to include nums[i]
+            subset.pop()
+            dfs(i + 1)
+
+        dfs(0)
+        return res
