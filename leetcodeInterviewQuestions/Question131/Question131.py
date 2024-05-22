778f425e69760b0ae2c729a6a4e0558a5484fb44
Stealeristaken <arda-orcun@hotmail.com>
2024-05-22 20:18:26 +0300

    daily added

---
+++ leetcodeInterviewQuestions/Question131/Question131.py
@@ -0,0 +1,47 @@
+from typing import List
+
+class Solution:
+
+    def is_pal(self, s: str) -> bool:
+        lo = 0
+        hi = len(s)-1
+        for i in range(len(s)//2):
+            if s[lo] != s[hi]: return False
+            lo += 1
+            hi -= 1
+        return True
+
+    def part_pal(self, s, l):
+        def backtrack(start, current_partition):
+            # If we've reached the end of the string, add the current partition to results
+            if start == len(s):
+                res.append(current_partition[:])
+                return
+            
+            # Iterate over the end indices of palindromes starting at the current index
+            for end in l[start]:
+                # Add the current palindrome to the partition
+                current_partition.append(s[start:end+1])
+                # Recur for the next part of the string
+                backtrack(end + 1, current_partition)
+                # Backtrack to try the next palindrome partition
+                current_partition.pop()
+
+        res = []
+        backtrack(0, [])
+        return res
+
+    def partition(self, s: str) -> List[List[str]]:
+        pals = [set([i]) for i in range(len(s))]
+        for i in range(len(s)):
+            for j in range(i, len(s)):
+                sub = s[i:j+1]
+                if self.is_pal(sub):
+                    pals[i].add(j)
+
+        res = self.part_pal(s, pals)
+
+        
+
+
+        return res
