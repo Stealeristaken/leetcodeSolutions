02c986b172d256dcc059ac193379d82f5650f74b
Stealeristaken <arda-orcun@hotmail.com>
2024-05-28 14:04:54 +0300

    more efficient way

--- leetcodeInterviewQuestions/Question2262/Question2262.py
+++ leetcodeInterviewQuestions/Question2262/Question2262.py
@@ -9,3 +9,17 @@ class Solution:
             dp.append(dp[-1]+sum(cur.values()))
 
         return dp[-1]
+  
+  
+  
+class Solution:
+    def appealSum(self, s: str) -> int:
+        last_pos = [0] * 26  # Initialize last positions for a-z
+        l = len(s)
+        total = 0
+        for idx, c in enumerate(s, 1):  # Start index from 1 for easier calculations
+            cv = ord(c) - 97
+            total += (idx - last_pos[cv]) * (l - idx + 1)
+            last_pos[cv] = idx
+
+        return total        
