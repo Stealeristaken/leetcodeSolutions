39298b88687a28f1fc4c3f5f1896ed7e5db81824
Stealeristaken <arda-orcun@hotmail.com>
2024-05-29 04:52:42 +0300

    cpp added

---
+++ leetcodeInterviewQuestions/Question1404/Question1404.cpp
@@ -0,0 +1,34 @@
+#include <string>
+using namespace std;
+
+class Solution
+{
+public:
+      int numSteps(string s)
+      {
+            int steps = 0;
+            int carry = 0;
+
+            for (int i = s.length() - 1; i > 0; --i)
+            {
+                  if (s[i] - '0' + carry == 1)
+                  {
+
+                        carry = 1;
+                        steps += 2;
+                  }
+                  else
+                  {
+
+                        steps += 1;
+                  }
+            }
+
+            if (carry == 1)
+            {
+                  steps += 1;
+            }
+
+            return steps;
+      }
+};
