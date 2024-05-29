23775db3c31e025d30c4bd6fdec03f1722627b54
Stealeristaken <arda-orcun@hotmail.com>
2024-05-29 04:50:01 +0300

    daily

--- leetcodeInterviewQuestions/Question1404/Question1404.py
+++ leetcodeInterviewQuestions/Question1404/Question1404.py
@@ -9,3 +9,29 @@ class Solution:
                 num //= 2
             c += 1
         return c
+  
+  
+  
+  
+class Solution:
+    def numSteps(self, s: str) -> int:
+        steps = 0
+
+        binary = list(s)
+        
+        while len(binary) > 1 or binary[0] != '1':
+            if binary[-1] == '0':
+                binary.pop()
+            else:
+                for i in range(len(binary)-1, -1, -1):
+                    if binary[i] == '1':
+                        binary[i] = '0'
+                    else:
+                        binary[i] = '1'
+                        break
+                else:
+                    binary.insert(0, '1')
+            
+            steps += 1
+        
+        return steps
