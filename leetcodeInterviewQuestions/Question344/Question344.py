3c8d8079f9792fb6878a5abbba93cc88dd1be87b
Stealeristaken <arda-orcun@hotmail.com>
2024-06-02 08:31:36 +0300

    sys

--- leetcodeInterviewQuestions/Question344/Question344.py
+++ leetcodeInterviewQuestions/Question344/Question344.py
@@ -12,3 +12,12 @@ class Solution:
             start += 1
             end -= 1
         return s
+  
+  
+  
+  
+with open("user.out", "w") as f:
+    for case in stdin:
+        f.write(f"{dumps(loads(case.strip())[::-1]).replace(', ', ',')}\n")
+exit()
+        
