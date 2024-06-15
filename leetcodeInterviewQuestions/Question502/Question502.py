b1ffab20d5719f9819257d7c345d51a20b814751
Stealeristaken <arda-orcun@hotmail.com>
2024-06-15 10:09:00 +0300

    brute

--- leetcodeInterviewQuestions/Question502/Question502.py
+++ leetcodeInterviewQuestions/Question502/Question502.py
@@ -16,3 +16,33 @@ class Solution:
                   current_capital += -heapq.heappop(max_heap)
             
             return current_capital  
+      
+      
+######################
+
+
+def findMaximizedCapital(k: int, w: int, profits: List[int], capital: List[int]) -> int:
+    if w > max(capital):
+        return w + sum(nlargest(k, profits))
+    maxProfit = []
+    minCapital = [(c, p) for c, p in zip(capital, profits)]
+    heapify(minCapital)
+    
+    for i in range(k):
+
+        while minCapital and minCapital[0][0] <= w :
+            c, p = heappop(minCapital)
+            heappush(maxProfit, -1 * p)
+
+        if not maxProfit :
+            break
+        w += -1 * heappop(maxProfit)
+
+    return w
+
+    
+if __name__ =='__main__' :
+    with open('user.out', 'w') as f :
+        for k, w, profits, capital in zip(map(loads, stdin), map(loads, stdin), map(loads, stdin), map(loads, stdin)) :
+            print(findMaximizedCapital(k, w, profits, capital), file = f)
+    exit()
