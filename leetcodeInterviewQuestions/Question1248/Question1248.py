240faee048dad16e70109506f6b0e20cd6291c7b
Stealeristaken <arda-orcun@hotmail.com>
2024-06-22 05:22:15 +0300

    daily added

--- leetcodeInterviewQuestions/Question1248/Question1248.py
+++ leetcodeInterviewQuestions/Question1248/Question1248.py
@@ -18,3 +18,39 @@ class Solution:
                 res += count_map[cnt] * count_map[cnt+k]
         
         return res
+
+###### O(N) solution ######
+
+class Solution:
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        z=0
+        start=0
+        ans=0
+        c=0
+        for i in range(len(nums)):
+            c+=nums[i]%2
+            while start<i and (nums[start]%2==0 or c>k):
+                if nums[start]%2:
+                    z=0
+                else:
+                    z+=1
+                c-=nums[start]%2
+                start+=1
+            if c==k:ans+=(z+1)
+        return ans
+
+def solve():
+    f = open('user.out', 'w')
+    iterator = map(loads, stdin)
+    while True:
+        try:
+            nums = next(iterator)
+            k = next(iterator)
+        except StopIteration:
+            break
+
+        print(solution.numberOfSubarrays(nums, k), file=f)
+
+solution = Solution()
+solve()
+exit()
