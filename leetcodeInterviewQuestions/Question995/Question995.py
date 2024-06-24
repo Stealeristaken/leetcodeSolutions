10467e6725ba9b2e7eab08ab5fb128f50ca90ab1
Stealeristaken <arda-orcun@hotmail.com>
2024-06-24 04:38:47 +0300

    daily added

--- leetcodeInterviewQuestions/Question995/Question995.py
+++ leetcodeInterviewQuestions/Question995/Question995.py
@@ -16,5 +16,24 @@ class Solution:
                 arr[index + k] -= 1
                 total += 1 
         return total
+##############################################################################################################
 
-        
+##O(N) time complexity
+
+
+class Solution:
+  def minKBitFlips(self, nums: List[int], k: int) -> int:
+    ans = 0
+    flippedTime = 0
+
+    for i, num in enumerate(nums):
+      if i >= k and nums[i - k] == 2:
+        flippedTime -= 1
+      if flippedTime % 2 == num:
+        if i + k > len(nums):
+          return -1
+        ans += 1
+        flippedTime += 1
+        nums[i] = 2
+
+    return ans
