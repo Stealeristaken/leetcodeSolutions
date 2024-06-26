e900eaf8132a5e9041451f80c43b3bc0f89fdb16
Stealeristaken <arda-orcun@hotmail.com>
2024-06-26 04:46:15 +0300

    daily added

---
+++ leetcodeInterviewQuestions/Question1382/Question1382.py
@@ -0,0 +1,31 @@
+# # Definition for a binary tree node.
+# # class TreeNode:
+# #     def __init__(self, val=0, left=None, right=None):
+# #         self.val = val
+# #         self.left = left
+# #         self.right = right
+class Solution:
+    # Time Complexity: O(n), inorder O(n) + buildBST O(n) =>  O(n)
+    # Space Complexity: O(n), nodes O(n) + recursive call O(logN) =>  O(n) 
+    def balanceBST(self, root: TreeNode) -> TreeNode:
+        nodes = []
+        self.inorder(root, nodes)
+        return self.buildBST(nodes, 0, len(nodes)-1)
+
+
+    def inorder(self, root: TreeNode, nodes):
+        if not root:
+            return 
+        self.inorder(root.left, nodes)
+        nodes.append(root)
+        self.inorder(root.right, nodes)
+
+    def buildBST(self, nodes, left, right) -> TreeNode:
+        if left > right:
+            return None
+        mid = (left + right)//2
+        node = nodes[mid]
+        node.left = self.buildBST(nodes, left, mid-1)
+        node.right = self.buildBST(nodes, mid+1, right)
+        return node
+            
