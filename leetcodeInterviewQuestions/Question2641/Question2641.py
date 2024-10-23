fc1106c6ba6de931eecb05d458dbb7ce48581fc3
Stealeristaken <arda-orcun@hotmail.com>
2024-10-23 04:37:06 +0300

    daily

---
+++ leetcodeInterviewQuestions/Question2641/Question2641.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+
+class Solution:
+    def replaceValueInTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return root
+        
+        q = deque()
+        root.val = 0
+        q.append(root)
+        while q:
+            n, levelSum = len(q), 0
+            for node in q:
+                levelSum += (0 if not node.left else node.left.val) + (0 if not node.right else node.right.val)
+            while n:
+                node = q.popleft()
+                val = levelSum - (0 if not node.left else node.left.val) - (0 if not node.right else node.right.val)
+                if node.left:
+                    node.left.val = val
+                    q.append(node.left)
+                if node.right:
+                    node.right.val = val
+                    q.append(node.right)
+                n -= 1
+        return root 
